cee4617cb7e7b0c69120d7aa1a36270d386766ba
Sagit7 <39763536+Sagit7@users.noreply.github.com>
2018-06-02 10:57:48 +0500

    Update and rename task2.rb to task1.rb

---
+++ task1.rb
@@ -0,0 +1,7 @@
+s = ARGV[0].downcase
+if  s.length >= 1  and s.length<=10000
+    s = s.delete ("р")
+    s = s.delete ("к")
+    puts S.delete ("н")
+end
+
--- task2.rb
+++
@@ -1,4 +0,0 @@
-S = ARGV[0].downcase
-S = S.delete ("р")
-S = S.delete ("к")
- puts S.delete ("н")
