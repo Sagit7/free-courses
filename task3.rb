20bee2df1a47383cdba891f11a76fb3933b29af8
Sagit7 <39763536+Sagit7@users.noreply.github.com>
2018-06-02 13:06:06 +0500

    2. Минимальный сдвиг

--- task3.rb
+++ task3.rb
@@ -1,9 +1,22 @@
 for i in 0 ... ARGV.length
-  if  ARGV[i].to_i < 1 || ARGV[i].to_i > 10000
-  exit;
+  if  ARGV[i].size < 1 || ARGV[i].size>10000
+  exit
   end
 end
-a = ARGV[0].to_i
-b = ARGV[1].to_i
-result =  a.gcd(b)
-puts result
+
+a = ARGV[0].split(""); b = ARGV[1].split(""); 
+
+i=0
+
+while a != b and i<=a.size
+      a.push(a[0])
+      a.delete_at(0)
+   i+=1
+end
+
+if a.length+1 != i
+   puts i
+else
+  puts '-1'
+end
+
